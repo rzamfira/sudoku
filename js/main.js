9640802aafec5d2e50429ac8e9e7a556657c409b
Radu-Andrei Zamfira <Radu.Andrei.Zamfira@stefanini.com>
2026-09-08 12:13:39 +0300

    feat: implement New Game button

--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
 import './state.js';
 import './puzzle.js';
 import { createLayout } from './gameLayout.js';
+import { newGameEventHandler } from './newGame.js';
 
 
 const puzzle = sudoku.generate('medium');  // generate the sudoku puzzle with medium difficulty
 createLayout(sudoku.board_string_to_grid(puzzle)); // create the app layout
+
+const newGameButton = document.querySelector('.new-game-button');
+newGameButton.addEventListener('click', () => {
+    newGameEventHandler();
+});
 
 
 
---
+++ js/newGame.js
@@ -0,0 +1,20 @@
+export function newGameEventHandler() {
+
+    const puzzle = sudoku.generate('medium');  // generate the sudoku puzzle with medium difficulty
+    const puzzleMatrix = sudoku.board_string_to_grid(puzzle);
+
+    puzzleMatrix.forEach((row, rowIndex) => {
+        row.forEach((value, columnIndex) => {
+
+            const cell = document.querySelector(`.grid-item[data-row-index="${rowIndex}"][data-column-index="${columnIndex}"]`);
+            if (value !== '.') {
+                cell.textContent = value;
+            }
+            else {
+                cell.textContent = '';
+            }
+
+        });
+    });
+
+}
